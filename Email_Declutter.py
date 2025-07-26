3b58c67c312ca02ab169ea30ba7c04ef544798d2
Aditya Veeramony <aditya.dfw@gmail.com>
2025-07-26 15:26:55 -0500

    Added Email and Password and loaded it in and code can now read credentials

--- Email_Declutter.py
+++ Email_Declutter.py
@@ -1,1 +1,12 @@
 # Decluttering an Email Project: Start-7/13/25
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+print(f"EMAIL={EMAIL}, PASSWORD={'set' if PASSWORD else 'not set'}")
+
